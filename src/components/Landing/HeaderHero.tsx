c82f82ace3f4dcdd45f7758a0ab4ac5818c8e012
Ayman Gouda <43366158+ayman7gouda@users.noreply.github.com>
2022-03-29 00:38:49 +1100

    Fix HeaderHero style

--- src/components/Landing/HeaderHero.tsx
+++ src/components/Landing/HeaderHero.tsx
@@ -18,26 +18,26 @@ export default function HeaderHero() {
       </header>
 
       <section className="container flex flex-col-reverse lg:flex-row items-center py-10 lg:py-14">
-        <div className="flex-1 w-[562px] items-start">
-          <h1 className="text-white text-[36px] leading-normal font-[800] text-left mb-16">
+        <div className="flex-1 items-start">
+          <h1 className="text-white text-[24px] lg:text-[36px] leading-normal font-[800] text-left mb-8 lg:mb-16 self-stretch">
             Take control of your career goals with
-            <a className="text-red-500"> JobFit </a>
+            <a className="text-cherry-red"> JobFit </a>
             <a className="hidden lg:inline">at Western Sydney University</a>
           </h1>
-          <p className="text-white text-[20px] text-left mb-28 leading-wsu">
+          <p className="text-white text-[16px] lg:text-[20px] text-left mb-8 lg:mb-28 leading-wsu">
             Explore and experiment with your university study journey to help
             you land the best job fit for your career goals and skills.
           </p>
-          <div className="flex flex-col lg:flex-row gap-3 mt-10">
-            <button type="button" className="flex-1 w-64 h-16 btn-red">
-              <div className="text-2xl text-white">Tryout JobFit Now</div>
+          <div className="flex flex-col lg:flex-row gap-3 lg:mt-10">
+            <button type="button" className="w-full lg:w-[264px] h-[33px] lg:h-[66px] btn-red">
+              <div className="text-xs lg:text-2xl text-bold text-white">Tryout JobFit Now</div>
             </button>
-            <button type="button" className="flex-1 w-64 h-16 btn-white">
-                <div className="text-2xl text-crimson">Program Offers</div>
+            <button type="button" className="w-full lg:w-[233px] h-[33px] lg:h-[66px] btn-white">
+                <div className="text-xs lg:text-2xl text-bold text-crimson">Program Offers</div>
             </button>
           </div>
         </div>
-        <div className="justify-center flex-1 z-10">
+        <div className="flex-1 z-10">
           <img src="./images/hero_01.png" alt="" className="" />
         </div>
       </section>
