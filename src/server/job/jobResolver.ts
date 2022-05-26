fa54a7f7d8d1425f776040be561deda9ca43709d
Tomas Trescak <tomi.trescak@gmail.com>
2022-05-26 14:00:20 +1000

    Added questionnaire result

--- src/server/job/jobResolver.ts
+++ src/server/job/jobResolver.ts
@@ -1,5 +1,9 @@
 import { JobCategory, Resolvers } from 'generated/serverTypes';
 
+import {
+  calculateDemand, extractJobSkills, findEmployers, getCategories, getJobs
+} from './jobHelpers';
+
 const avg = (arr) => arr.reduce((acc, v, i, a) => acc + v / a.length, 0);
 const sum = (arr) => arr.reduce((a, b) => a + b);
 
@@ -38,6 +42,50 @@ export const jobResolvers: Resolvers = {
       // const result = await db.$queryRaw<Job[]>`
       // SELECT * FROM Job WHERE minAnnualSalary IS NOT NULL AND FLOOR(ANZSCOCode / ${division}) = ${id} LIMIT 500`;
       return result;
+    },
+    async jobRoleProfile(_, { unit }, { db }) {
+      const jobs = await getJobs(db, unit);
+      const categories = await getCategories(db);
+
+      // this one is for the description
+      const anzsco = await db.aNZSCO.findFirst({ where: { id: unit } });
+
+      const jobCount = jobs.length;
+
+      // find who are the employers
+      const employers = await findEmployers(db, unit);
+
+      // demand in comparison to other jobs
+      const average =
+        categories.reduce((p, n) => p + n._count.unit, 0) / categories.length;
+      const demand = calculateDemand(jobCount, average);
+
+      // find the projection for the current job
+      const projection = await db.jobProjections.findFirst({
+        where: { id: unit },
+      });
+
+      // the category produces averages and min and max salary
+      let category = categories.find((c) => c.unit === unit);
+
+      // aggregate skills
+      const skills = extractJobSkills(jobs);
+
+      return {
+        name: anzsco.name,
+        description: anzsco.description,
+        demand,
+        jobCount,
+        average,
+        minSalary: category._min.maxAnnualSalary,
+        maxSalary: category._max.maxAnnualSalary,
+        avgSalary: category._avg.maxAnnualSalary,
+        employers: employers
+          .slice(0, 10)
+          .map((e) => ({ name: e.employer, count: e._count.employer })),
+        projection: projection?.growth,
+        skills,
+      };
     },
     async jobCategories(_, _args, { db }) {
       const result = await db.$queryRaw<JobCategory[]>`
