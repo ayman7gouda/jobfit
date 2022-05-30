e95a2adc156934a2057676a36e8ba8806ec7314a
Tomas Trescak <tomi.trescak@gmail.com>
2022-05-30 16:31:38 +1000

    check skills

--- src/pages/admin/skills.tsx
+++ src/pages/admin/skills.tsx
@@ -1,8 +1,18 @@
 import type { NextPage } from "next";
 
+import { useEffect, useState } from 'react';
+
 import { Layout } from 'components/Admin/Skills/Layout';
 
 const Home: NextPage = () => {
+  const [ready, setReady] = useState(false);
+
+  useEffect(() => setReady(true), []);
+
+  if (!ready) {
+    return <span></span>;
+  }
+
   return (
     <Layout>
       <div className="pt-4 px-4">Please choose a skill category</div>
