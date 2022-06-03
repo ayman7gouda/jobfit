e4dd0b7f0a8c11b056e05c39684e95547af798f8
Tomas Trescak <tomi.trescak@gmail.com>
2022-06-03 23:11:10 +1000

    Ready to go!

--- src/pages/admin/handbook/specialisations/[name].tsx
+++ src/pages/admin/handbook/specialisations/[name].tsx
@@ -1,28 +1,40 @@
-import { useEffect, useState } from "react";
+import {
+  DndProvider,
+  getBackendOptions,
+  MultiBackend,
+} from "@minoru/react-dnd-treeview";
 
 import { daoInNode, daoOutNode } from "components/Admin/Handbook/helpers";
 import { useSaveSpecialisationHandbookMutation } from "components/Admin/Handbook/queries/saveSpecialisationHandbook.mutation.generated";
 import { useSpecialisationQuery } from "components/Admin/Handbook/queries/specialisation.query.generated";
-import { Layout } from "components/Admin/Handbook/TreeView";
-import { initGuid } from "components/Admin/Handbook/types";
+import { Layout, TreeView } from "components/Admin/Handbook/TreeView";
+import { initGuid, Option } from "components/Admin/Handbook/types";
 import { useRouter } from "next/router";
 
-const ClientOnly = ({ children }: any) => {
-  const [isClient, setClient] = useState(false);
+// const ClientOnly = ({ children }: any) => {
+//   const [isClient, setClient] = useState(false);
 
-  useEffect(() => setClient(true), []);
+//   useEffect(() => setClient(true), []);
 
-  if (!isClient) {
-    return null;
-  }
-  return children;
-};
+//   if (!isClient) {
+//     return null;
+//   }
+//   return children;
+// };
 
-export function SpecialisationContainer() {
-  const { id } = useRouter().query;
-  const numId = parseInt(id as string);
+export function SpecialisationContainer({
+  all,
+  programOptions,
+  majorOptions,
+  id,
+}: {
+  programOptions: Option[];
+  majorOptions: Option[];
+  all: Option[];
+  id: number;
+}) {
   const { data, loading } = useSpecialisationQuery({
-    variables: { id: numId },
+    variables: { id },
   });
   const [save] = useSaveSpecialisationHandbookMutation();
 
@@ -38,10 +50,13 @@ export function SpecialisationContainer() {
   initGuid(tree);
 
   return (
-    <Layout
+    <TreeView
+      key={id}
+      defaultTree={tree.handbook}
       model={tree}
-      id={numId}
-      part="majors"
+      all={all}
+      programOptions={programOptions}
+      majorOptions={majorOptions}
       save={(h) => {
         save({
           variables: {
@@ -56,15 +71,41 @@ export function SpecialisationContainer() {
   );
 }
 
+export const Page = () => {
+  const { id } = useRouter().query;
+  const numId = parseInt(id as string);
+
+  return (
+    <>
+      <Layout
+        id={numId}
+        part="majors"
+        treeView={({ programOptions, majorOptions, all }) => (
+          <SpecialisationContainer
+            programOptions={programOptions}
+            majorOptions={majorOptions}
+            all={all}
+            id={numId}
+          />
+        )}
+      />
+      <style jsx global>{`
+        body,
+        html,
+        #__next {
+          height: 100%;
+        }
+      `}</style>
+    </>
+  );
+};
+
 export default () => (
-  <ClientOnly>
-    <SpecialisationContainer />
-    <style jsx global>{`
-      body,
-      html,
-      #__next {
-        height: 100%;
-      }
-    `}</style>
-  </ClientOnly>
+  <DndProvider
+    backend={MultiBackend}
+    debugMode={true}
+    options={getBackendOptions()}
+  >
+    <Page />
+  </DndProvider>
 );
